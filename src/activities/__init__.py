f6eddc90766e42dc0bb1a9712a4c236d6eac0474
Alex Ganose <alexganose@googlemail.com>
2021-02-26 19:21:27 -0800

    Update init

--- src/activities/__init__.py
+++ src/activities/__init__.py
@@ -3,5 +3,8 @@ from activities.core.maker import Maker
 from activities.core.outputs import Outputs
 from activities.core.reference import Reference
 from activities.core.task import Task, task
+from activities.core.util import initialize_logger
 
-__all__ = ["Activity", "Reference", "Outputs", "Task", "task", "Maker"]
+__all__ = [
+    "Activity", "Reference", "Outputs", "Task", "task", "Maker", "initialize_logger"
+]
