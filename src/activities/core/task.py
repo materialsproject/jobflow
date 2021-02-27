cc0487745f47067ef3b8000a62f5851ba728721f
Alex Ganose <alexganose@googlemail.com>
2021-02-26 17:27:46 -0800

    Allow running tasks

--- src/activities/core/task.py
+++ src/activities/core/task.py
@@ -1,12 +1,18 @@
+import warnings
 from dataclasses import dataclass, field
-from typing import Any, Dict, Optional, Tuple
+from typing import Any, Dict, Hashable, Optional, Sequence, Tuple
 from uuid import UUID, uuid4
 
-from monty.json import MontyEncoder, MSONable
+from maggma.core import Store as MaggmaStore
+from monty.json import MSONable
 
 from activities.core.base import HasInputOutput
 from activities.core.outputs import Outputs
-from activities.core.reference import Reference
+from activities.core.reference import (
+    Reference,
+    find_and_resolve_references,
+    find_references,
+)
 
 
 def task(method=None, outputs=None):
@@ -25,12 +31,13 @@ def task(method=None, outputs=None):
 
         return get_task
 
-    if method:
-        # This was an actual decorator call, ex: @cached_property
-        return decorator(method)
-    else:
-        # This is a factory call, ex: @cached_property()
+    # See if we're being called as @task or @task().
+    if method is None:
+        # We're called with parens.
         return decorator
+
+    # We're called as @task without parens.
+    return decorator(method)
 
 
 @dataclass
@@ -44,14 +51,14 @@ class Task(HasInputOutput, MSONable):
 
     def __post_init__(self):
         if self.outputs:
-            self.outputs = self.outputs.reference(self.uuid)
+            self.outputs = self.outputs.to_reference(self.uuid)
 
     def __call__(self, *args, **kwargs):
         return Task(
             function=self.function,
             args=args,
             kwargs=kwargs,
-            outputs=self.outputs.reference(self.uuid),
+            outputs=self.outputs.to_reference(self.uuid),
         )
 
     @property
@@ -71,27 +78,135 @@ class Task(HasInputOutput, MSONable):
             return tuple()
         return self.outputs.references
 
+    def run(
+        self,
+        output_store: Optional[MaggmaStore] = None,
+        output_cache: Optional[Dict[UUID, Dict[str, Any]]] = None,
+    ) -> "TaskResponse":
+        from importlib import import_module
 
-def find_references(arg: Any) -> Tuple[Reference, ...]:
-    import json
+        module = import_module(self.function[0])
+        function = getattr(module, self.function[1], None)
 
-    from pydash import get
+        if function is None:
+            raise ValueError(f"Could not import {function} from {module}")
 
-    from activities.core.util import find_key_value
+        args, kwargs = resolve_args(
+            self.args, self.kwargs, output_store=output_store, output_cache=output_cache
+        )
+        all_returned_data = function(*args, **kwargs)
+        return TaskResponse.from_task_returns(all_returned_data, type(self.outputs))
 
-    if isinstance(arg, Reference):
-        # if the argument is a reference then stop there
-        return tuple([arg])
 
-    elif isinstance(arg, (float, int, str, bool)):
-        # argument is a primitive, we won't find a reference here
-        return tuple()
+def resolve_args(
+    args: Tuple[Any, ...],
+    kwargs: Dict[str, Any],
+    output_store: Optional[MaggmaStore] = None,
+    output_cache: Optional[Dict[UUID, Dict[str, Any]]] = None,
+) -> Tuple[Sequence[Any, ...], Dict[str, Any]]:
+    resolved_args = []
+    for arg in args:
+        resolved_arg = find_and_resolve_references(
+            arg, output_store=output_store, output_cache=output_cache
+        )
+        resolved_args.append(resolved_arg)
 
-    # otherwise, serialize the argument to a dictionary
-    arg = json.loads(MontyEncoder().encode(arg))
+    resolved_kwargs = find_and_resolve_references(
+        kwargs, output_store=output_store, output_cache=output_cache
+    )
 
-    # recursively find any reference classes
-    locations = find_key_value(arg, "@class", "Reference")
+    return resolved_args, resolved_kwargs
 
-    # deserialize references and return
-    return tuple([Reference.from_dict(get(arg, loc)) for loc in locations])
+
+@dataclass
+class Store:
+    data: Dict[Hashable, Any]
+
+
+@dataclass
+class Detour:
+    from activities.core.activity import Activity
+
+    activity: Activity
+
+
+@dataclass
+class Restart:
+    from activities.core.activity import Activity
+
+    activity: Activity
+
+
+@dataclass
+class Exit:
+    exit_type: bool
+
+
+@dataclass
+class TaskResponse:
+    # TODO: Consider merging this with TaskResponse
+    from activities.core.activity import Activity
+
+    outputs: Optional[Outputs] = None
+    detour: Optional[Activity] = None
+    restart: Optional[Activity] = None
+    store: Optional[Dict[str, Any]] = None
+    exit: bool = False
+
+    @classmethod
+    def from_task_returns(
+        cls,
+        task_returns: Optional[Any],
+        task_output_class: Optional[str],
+    ) -> "TaskResponse":
+        from collections import defaultdict
+
+        if task_returns is None:
+            return TaskResponse()
+        elif not isinstance(task_returns, (float, tuple)):
+            task_returns = [task_returns]
+
+        to_parse = defaultdict(list)
+        for returned_data in task_returns:
+            if isinstance(returned_data, Outputs):
+                to_parse[Outputs].append(returned_data)
+            elif isinstance(returned_data, (Detour, Restart, Store, Exit)):
+                to_parse[type(returned_data)].append(returned_data)
+            else:
+                raise ValueError(
+                    f"Unrecognised return type: {type(returned_data)}. Must be one of: "
+                    "Output, Detour, Restart, Store, Exit}"
+                )
+
+        task_response_data = {}
+        for return_type, data in to_parse.items():
+            data = data[0]
+
+            if len(data) > 1:
+                raise ValueError(
+                    f"Only one {return_type} object can be returned per task."
+                )
+
+            if return_type == Outputs and task_output_class is None:
+                warnings.warn(
+                    "Task returned outputs but none were expected. "
+                    "Outputs will be ignored"
+                )
+            elif return_type == Outputs:
+                if type(data) != task_output_class:
+                    warnings.warn(
+                        f"Output class returned by task {type(data)} does "
+                        f"not match expected output class {task_output_class}."
+                    )
+                task_response_data["output"] = data
+
+            elif return_type == Store:
+                task_response_data["store"] = data.data
+            elif return_type == Detour:
+                task_response_data["detour"] = data.activity
+            elif return_type == Restart:
+                task_response_data["detour"] = data.activity
+            elif return_type == Exit:
+                task_response_data["exit"] = data.exit_type
+
+        return cls(**task_response_data)
