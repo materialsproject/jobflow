def7b9e1ca96e65d92458630c30191fa46d7fad0
Alex Ganose <alexganose@googlemail.com>
2021-03-01 19:24:02 -0800

    Update detour example

--- examples/detours.py
+++ examples/detours.py
@@ -8,39 +8,39 @@ from activities.managers.local import run_activity_locally
 # define task and outputs to generate letters of the alphabet
 
 @dataclass
-class AlphabetOutputs(Outputs):
-    letters: List[str]
+class ListOfStrings(Outputs):
+    strings: List[str]
 
 
-@task(outputs=AlphabetOutputs)
+@task(outputs=ListOfStrings)
 def generate_alphabet():
     import string
-    return AlphabetOutputs(list(string.ascii_lowercase))
+    return ListOfStrings(list(string.ascii_lowercase))
 
 
-# define task, outputs and, activity to repeat a letter 5 times
+# define task, outputs and, activity to repeat a string 5 times
 
 @dataclass
-class RepeatOutputs(Outputs):
-    repeated_letters: str
+class String(Outputs):
+    string: str
 
 
-@task(outputs=RepeatOutputs)
-def repeat_letter(letter: str):
-    return RepeatOutputs(letter * 5)
+@task(outputs=String)
+def repeat_string(string: str):
+    return String(string * 5)
 
 
-def get_repeat_activity(letter: str):
-    repeat_letter_task = repeat_letter(letter)
-    return Activity("Repeat", [repeat_letter_task], repeat_letter_task.outputs)
+def get_repeat_activity(string: str):
+    repeat_string_task = repeat_string(string)
+    return Activity("Repeat", [repeat_string_task], repeat_string_task.outputs)
 
 
-# define a task to generate repeat activities for multiple letters
+# define a task to generate repeat activities for multiple strings
 
-@task(outputs=AlphabetOutputs)
-def repeat_letters(letters: List[str]):
-    activities = [get_repeat_activity(letter) for letter in letters]
-    outputs = AlphabetOutputs([a.outputs.repeated_letters for a in activities])
+@task(outputs=ListOfStrings)
+def repeat_strings(strings: List[str]):
+    activities = [get_repeat_activity(string) for string in strings]
+    outputs = ListOfStrings([activity.outputs.string for activity in activities])
     detour_activity = Activity("Repeats", activities, outputs)
     return Detour(detour_activity)
 
@@ -48,7 +48,7 @@ def repeat_letters(letters: List[str]):
 # create an activity that will generate the letters of the alphabet, then submit
 # a new activity for each letter to repeat it 5 times.
 alphabet = generate_alphabet()
-repeat = repeat_letters(letters=alphabet.outputs.letters)
+repeat = repeat_strings(strings=alphabet.outputs.strings)
 my_activity = Activity("Repeat alphabet", [alphabet, repeat], repeat.outputs)
 
 # run the activity
--- src/activities/__init__.py
+++ src/activities/__init__.py
@@ -2,9 +2,10 @@ from activities.core.activity import Activity
 from activities.core.maker import Maker
 from activities.core.outputs import Outputs
 from activities.core.reference import Reference
-from activities.core.task import Task, task
+from activities.core.task import Task, task, Detour, Store, Stop
 from activities.core.util import initialize_logger
 
 __all__ = [
-    "Activity", "Reference", "Outputs", "Task", "task", "Maker", "initialize_logger"
+    "Activity", "Reference", "Outputs", "Task", "Detour", "Store", "Stop",
+    "task", "Maker", "initialize_logger"
 ]
