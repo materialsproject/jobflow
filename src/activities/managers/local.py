3d614b8a5ed3ea270090e85f08b2e06724e85340
Alex Ganose <alexganose@googlemail.com>
2021-03-01 18:55:30 -0800

    Add local runner support for detours and stopping

--- src/activities/managers/local.py
+++ src/activities/managers/local.py
@@ -7,32 +7,54 @@ logger = logging.getLogger(__name__)
 
 
 def run_activity_locally(activity: Activity):
-    logger.info(f"Started executing activities locally")
-
     output_cache = {}
-    activity: Activity
-    for activity, parents in activity.iteractivity():
-        if (
-            activity.contains_activities
-            and len(parents) == 0
-            and activity.outputs is None
-        ):
-            # activity is a container only so we don't need to do anything
-            logger.info(
-                f"{activity.name} activity has no outputs and no tasks, skipping..."
-            )
-            continue
+    store = {}
+    stopped_parents = set()
 
-        outputs = activity.run(output_cache=output_cache)
+    def _run(root_activity):
+        subactivity: Activity
+        for subactivity, parents in root_activity.iteractivity():
+            if len(set(parents).intersection(stopped_parents)) > 0:
+                # stop children has been called for one of the activities' parents
+                logger.info(
+                    f"{subactivity.name} is a child of an activity with "
+                    f"stop_children=True, skipping..."
+                )
+                stopped_parents.add(subactivity.uuid)
+                continue
 
-        if outputs.restart is not None:
-            pass
+            if (
+                subactivity.contains_activities
+                and len(parents) == 0
+                and subactivity.outputs is None
+            ):
+                # subactivity is a container only so we don't need to do anything
+                logger.info(
+                    f"{subactivity.name} activity has no outputs and no tasks, "
+                    "skipping..."
+                )
+                continue
 
-        if outputs.detour is not None:
-            pass
+            response = subactivity.run(output_cache=output_cache)
 
-        if outputs.store is not None:
-            pass
+            if response.store is not None:
+                store[subactivity.uuid] = response.store
 
+            if response.stop_children:
+                stopped_parents.add(subactivity.uuid)
+
+            if response.stop_activities:
+                break
+
+            if response.detour is not None:
+                detour_activity, remaining_tasks = response.detour
+                _run(detour_activity)
+                _run(remaining_tasks)
+
+            if response.restart is not None:
+                pass
+
+    logger.info(f"Started executing activities locally")
+    _run(activity)
     logger.info(f"Finished executing activities locally")
     return output_cache
