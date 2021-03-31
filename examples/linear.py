7fd27ba72c8782a3cb0b1e2e840538e19252b89d
Alex Ganose <alexganose@googlemail.com>
2021-03-31 16:04:36 -0700

    Improve linear example

--- examples/linear.py
+++ examples/linear.py
@@ -9,7 +9,10 @@ def add(a, b):
 add_job_first = add(1, 2)
 add_job_second = add(4, 6)
 
-my_activity = Activity(jobs=[add_job_first, add_job_second], order=JobOrder.LINEAR)
+# No edges between the tasks as the tasks do not depend on each other
+auto_activity = Activity(jobs=[add_job_first, add_job_second], order=JobOrder.AUTO)
+auto_activity.draw_graph().show()
 
-# activity graph shows an edge between the jobs even though there are no shared outputs
-my_activity.draw_graph().show()
+# activity graph now shows an edge between the jobs due to the linear execution order
+linear_activity = Activity(jobs=[add_job_first, add_job_second], order=JobOrder.LINEAR)
+linear_activity.draw_graph().show()
