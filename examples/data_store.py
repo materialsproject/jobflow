ac8ebeee82f9b8cb6bafe1b52d8f442ea82b9b82
Alex Ganose <alexganose@googlemail.com>
2021-04-06 18:35:30 -0700

    Add data store example

---
+++ examples/data_store.py
@@ -0,0 +1,21 @@
+from activities import job, Activity, run_locally
+
+
+@job(data=True)
+def generate_big_data():
+    data = list(range(1000))
+    return data
+
+
+@job
+def calculate_sum(numbers):
+    return sum(numbers)
+
+
+big_data_job = generate_big_data()
+sum_job = calculate_sum(big_data_job.output)
+act = Activity([big_data_job, sum_job])
+
+output = run_locally(act)
+print(output)
+
