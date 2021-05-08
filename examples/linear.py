bf79422bd306bac0d53d86ba0555eb7ebfd3059f
Alex Ganose <alexganose@googlemail.com>
2021-05-08 12:11:53 -0700

    Update linear example

--- examples/linear.py
+++ examples/linear.py
@@ -6,13 +6,14 @@ def add(a, b):
     return a + b
 
 
+# No edges between the jobs as they do not depend on each other
 add_job_first = add(1, 2)
 add_job_second = add(4, 6)
-
-# No edges between the tasks as the tasks do not depend on each other
 auto_flow = Flow([add_job_first, add_job_second], order=JobOrder.AUTO)
 auto_flow.draw_graph().show()
 
 # flow graph now shows an edge between the jobs due to the linear execution order
+add_job_first = add(1, 2)
+add_job_second = add(4, 6)
 linear_flow = Flow([add_job_first, add_job_second], order=JobOrder.LINEAR)
 linear_flow.draw_graph().show()
