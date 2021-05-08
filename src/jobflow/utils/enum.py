a55eeee9f2fdd1f21d4fe447e4b81b101f0a6022
Alex Ganose <alexganose@googlemail.com>
2021-05-08 00:44:17 -0700

    Document enum

--- src/jobflow/utils/enum.py
+++ src/jobflow/utils/enum.py
@@ -6,23 +6,25 @@ __all__ = ["ValueEnum"]
 
 
 class ValueEnum(Enum):
-    """
-    Enum that serializes to string as the value and can be compared against a string.
-    """
+    """Enum that serializes to string as the value and can be compared against a str."""
 
     def __str__(self):
+        """Get a string representation of the enum."""
         return str(self.value)
 
     def __eq__(self, other):
+        """Compare to another enum for equality."""
         if type(self) == type(other) and self.value == other.value:
             return True
         else:
             return str(self.value) == str(other)
 
     def as_dict(self):
+        """Create a serializable representation of the enum."""
         return str(self.value)
 
     def __new__(cls, value, doc=None):
+        """Add support for enum docstrings."""
         self = object.__new__(cls)
         self._value_ = value
         if doc is not None:
