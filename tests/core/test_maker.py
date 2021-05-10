20a0e04e06624de1e0765360f64389f3611af08f
Alex Ganose <alexganose@googlemail.com>
2021-05-10 16:18:02 -0700

    WIP: Maker tests

--- tests/core/test_maker.py
+++ tests/core/test_maker.py
@@ -12,3 +12,23 @@ def test_bad_subclass():
 
     with pytest.raises(NotImplementedError):
         BadMaker().make()
+
+
+def test_job_maker():
+    from dataclasses import dataclass
+
+    from jobflow.core.job import job
+    from jobflow.core.maker import Maker
+
+    @dataclass
+    class AddMaker(Maker):
+        name: str = "add"
+
+        @job
+        def make(self, a, b):
+            return a + b
+
+    maker = AddMaker()
+    add_job = maker.make(1, 2)
+    assert add_job.name == "add"
+    assert add_job.function == maker.make
