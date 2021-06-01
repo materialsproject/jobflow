6370bd15525bc6d68612e15f002e98dc647f1f58
Alex Ganose <alexganose@googlemail.com>
2021-06-01 18:01:24 +0100

    TST: Use memory jobstore for all tests

--- tests/managers/test_local.py
+++ tests/managers/test_local.py
@@ -14,7 +14,7 @@ def test_simple_job(memory_jobstore, clean_dir, simple_job, capsys):
     assert result["output"] == "12345_end"
 
 
-def test_simple_flow(mongo_jobstore, clean_dir, simple_flow, capsys):
+def test_simple_flow(memory_jobstore, clean_dir, simple_flow, capsys):
     from pathlib import Path
 
     from jobflow import run_locally
@@ -23,19 +23,19 @@ def test_simple_flow(mongo_jobstore, clean_dir, simple_flow, capsys):
     uuid = flow.jobs[0].uuid
 
     # run without log
-    run_locally(flow, store=mongo_jobstore, log=False)
+    run_locally(flow, store=memory_jobstore, log=False)
     captured = capsys.readouterr()
     assert "INFO Started executing jobs locally" not in captured.out
     assert "INFO Finished executing jobs locally" not in captured.out
 
     # run with log
-    responses = run_locally(flow, store=mongo_jobstore)
+    responses = run_locally(flow, store=memory_jobstore)
 
     # check responses has been filled
     assert responses[uuid][1].output == "12345_end"
 
     # check store has the activity output
-    result = mongo_jobstore.query_one({"uuid": uuid})
+    result = memory_jobstore.query_one({"uuid": uuid})
     assert result["output"] == "12345_end"
 
     # check no folders were written
@@ -48,13 +48,13 @@ def test_simple_flow(mongo_jobstore, clean_dir, simple_flow, capsys):
     assert "INFO Finished executing jobs locally" in captured.out
 
     # run with folders
-    responses = run_locally(flow, store=mongo_jobstore, create_folders=True)
+    responses = run_locally(flow, store=memory_jobstore, create_folders=True)
     assert responses[uuid][1].output == "12345_end"
     folders = list(Path(".").glob("job_*/"))
     assert len(folders) == 1
 
 
-def test_connected_flow(mongo_jobstore, clean_dir, connected_flow, capsys):
+def test_connected_flow(memory_jobstore, clean_dir, connected_flow, capsys):
     from jobflow import run_locally
 
     flow = connected_flow()
@@ -62,7 +62,7 @@ def test_connected_flow(mongo_jobstore, clean_dir, connected_flow, capsys):
     uuid2 = flow.jobs[1].uuid
 
     # run with log
-    responses = run_locally(flow, store=mongo_jobstore)
+    responses = run_locally(flow, store=memory_jobstore)
 
     # check responses has been filled
     assert len(responses) == 2
@@ -70,14 +70,14 @@ def test_connected_flow(mongo_jobstore, clean_dir, connected_flow, capsys):
     assert responses[uuid2][1].output == "12345_end_end"
 
     # check store has the activity output
-    result1 = mongo_jobstore.query_one({"uuid": uuid1})
-    result2 = mongo_jobstore.query_one({"uuid": uuid2})
+    result1 = memory_jobstore.query_one({"uuid": uuid1})
+    result2 = memory_jobstore.query_one({"uuid": uuid2})
 
     assert result1["output"] == "12345_end"
     assert result2["output"] == "12345_end_end"
 
 
-def test_nested_flow(mongo_jobstore, clean_dir, nested_flow, capsys):
+def test_nested_flow(memory_jobstore, clean_dir, nested_flow, capsys):
     from jobflow import run_locally
 
     flow = nested_flow()
@@ -87,7 +87,7 @@ def test_nested_flow(mongo_jobstore, clean_dir, nested_flow, capsys):
     uuid4 = flow.jobs[1].jobs[1].uuid
 
     # run with log
-    responses = run_locally(flow, store=mongo_jobstore)
+    responses = run_locally(flow, store=memory_jobstore)
 
     # check responses has been filled
     assert len(responses) == 4
@@ -97,10 +97,10 @@ def test_nested_flow(mongo_jobstore, clean_dir, nested_flow, capsys):
     assert responses[uuid4][1].output == "12345_end_end_end_end"
 
     # check store has the activity output
-    result1 = mongo_jobstore.query_one({"uuid": uuid1})
-    result2 = mongo_jobstore.query_one({"uuid": uuid2})
-    result3 = mongo_jobstore.query_one({"uuid": uuid3})
-    result4 = mongo_jobstore.query_one({"uuid": uuid4})
+    result1 = memory_jobstore.query_one({"uuid": uuid1})
+    result2 = memory_jobstore.query_one({"uuid": uuid2})
+    result3 = memory_jobstore.query_one({"uuid": uuid3})
+    result4 = memory_jobstore.query_one({"uuid": uuid4})
 
     assert result1["output"] == "12345_end"
     assert result2["output"] == "12345_end_end"
@@ -108,14 +108,14 @@ def test_nested_flow(mongo_jobstore, clean_dir, nested_flow, capsys):
     assert result4["output"] == "12345_end_end_end_end"
 
 
-def test_addition_flow(mongo_jobstore, clean_dir, addition_flow, capsys):
+def test_addition_flow(memory_jobstore, clean_dir, addition_flow, capsys):
     from jobflow import run_locally
 
     flow = addition_flow()
     uuid1 = flow.jobs[0].uuid
 
     # run with log
-    responses = run_locally(flow, store=mongo_jobstore)
+    responses = run_locally(flow, store=memory_jobstore)
     uuid2 = [u for u in responses.keys() if u != uuid1][0]
 
     # check responses has been filled
@@ -125,14 +125,14 @@ def test_addition_flow(mongo_jobstore, clean_dir, addition_flow, capsys):
     assert responses[uuid2][1].output == "11_end"
 
     # check store has the activity output
-    result1 = mongo_jobstore.query_one({"uuid": uuid1})
-    result2 = mongo_jobstore.query_one({"uuid": uuid2})
+    result1 = memory_jobstore.query_one({"uuid": uuid1})
+    result2 = memory_jobstore.query_one({"uuid": uuid2})
 
     assert result1["output"] == 11
     assert result2["output"] == "11_end"
 
 
-def test_detour_flow(mongo_jobstore, clean_dir, detour_flow, capsys):
+def test_detour_flow(memory_jobstore, clean_dir, detour_flow, capsys):
     from jobflow import run_locally
 
     flow = detour_flow()
@@ -140,7 +140,7 @@ def test_detour_flow(mongo_jobstore, clean_dir, detour_flow, capsys):
     uuid3 = flow.jobs[1].uuid
 
     # run with log
-    responses = run_locally(flow, store=mongo_jobstore)
+    responses = run_locally(flow, store=memory_jobstore)
     uuid2 = [u for u in responses.keys() if u != uuid1 and u != uuid3][0]
 
     # check responses has been filled
@@ -151,9 +151,9 @@ def test_detour_flow(mongo_jobstore, clean_dir, detour_flow, capsys):
     assert responses[uuid3][1].output == "12345_end"
 
     # check store has the activity output
-    result1 = mongo_jobstore.query_one({"uuid": uuid1})
-    result2 = mongo_jobstore.query_one({"uuid": uuid2})
-    result3 = mongo_jobstore.query_one({"uuid": uuid3})
+    result1 = memory_jobstore.query_one({"uuid": uuid1})
+    result2 = memory_jobstore.query_one({"uuid": uuid2})
+    result3 = memory_jobstore.query_one({"uuid": uuid3})
 
     assert result1["output"] == 11
     assert result2["output"] == "11_end"
@@ -163,7 +163,7 @@ def test_detour_flow(mongo_jobstore, clean_dir, detour_flow, capsys):
     assert result2["completed_at"] < result3["completed_at"]
 
 
-def test_replace_flow(mongo_jobstore, clean_dir, replace_flow, capsys):
+def test_replace_flow(memory_jobstore, clean_dir, replace_flow, capsys):
     from jobflow import run_locally
 
     flow = replace_flow()
@@ -171,7 +171,7 @@ def test_replace_flow(mongo_jobstore, clean_dir, replace_flow, capsys):
     uuid2 = flow.jobs[1].uuid
 
     # run with log
-    responses = run_locally(flow, store=mongo_jobstore)
+    responses = run_locally(flow, store=memory_jobstore)
 
     # check responses has been filled
     assert len(responses) == 2
@@ -182,9 +182,9 @@ def test_replace_flow(mongo_jobstore, clean_dir, replace_flow, capsys):
     assert responses[uuid2][1].output == "12345_end"
 
     # check store has the activity output
-    result1 = mongo_jobstore.query_one({"uuid": uuid1, "index": 1})
-    result2 = mongo_jobstore.query_one({"uuid": uuid1, "index": 2})
-    result3 = mongo_jobstore.query_one({"uuid": uuid2, "index": 1})
+    result1 = memory_jobstore.query_one({"uuid": uuid1, "index": 1})
+    result2 = memory_jobstore.query_one({"uuid": uuid1, "index": 2})
+    result3 = memory_jobstore.query_one({"uuid": uuid2, "index": 1})
 
     assert result1["output"] == 11
     assert result2["output"] == "11_end"
