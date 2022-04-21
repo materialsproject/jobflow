c3063bb6d550ace28c4a1fc9bbc88b469ad5ec9f
Alex Ganose <alexganose@googlemail.com>
2022-04-21 14:34:20 +0100

    Fix version

--- src/jobflow/_version.py
+++ src/jobflow/_version.py
@@ -1,7 +1,7 @@
 from pkg_resources import DistributionNotFound, get_distribution
 
 try:
-    __version__ = get_distribution(__name__).version
+    __version__ = get_distribution("jobflow").version
 except DistributionNotFound:
     # package is not installed
     __version__ = ""
