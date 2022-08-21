feb2c15b52c8cc0b950fb2bbd81358a2809f1c5d
Alex Ganose <utf@users.noreply.github.com>
2022-08-21 19:36:59 +0100

    Update m2r2 and sphinx

--- setup.py
+++ setup.py
@@ -34,9 +34,9 @@ setup(
     ],
     extras_require={
         "docs": [
-            "sphinx==5.0.2",
+            "sphinx==5.1.1",
             "furo==2022.6.21",
-            "m2r2==0.3.2",
+            "m2r2==0.3.3",
             "ipython==8.4.0",
             "nbsphinx==0.8.9",
             "nbsphinx-link==1.3.0",
