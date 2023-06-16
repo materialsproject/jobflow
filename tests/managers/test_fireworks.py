cff929479b797c638476cb55d9190376f9eaf411
Eric Sivonxay <esivonxay@lbl.gov>
2023-06-16 11:46:55 -0700

    No need to check the fw_id, since it's already been tested

--- tests/managers/test_fireworks.py
+++ tests/managers/test_fireworks.py
@@ -157,7 +157,9 @@ def test_simple_flow_no_store(lpad, fw_dir, simple_flow, capsys):
     assert "INFO Finished job - func" in captured.out
 
 
-def test_simple_flow_metadata(lpad, mongo_jobstore, fw_dir, simple_flow, connected_flow, capsys):
+def test_simple_flow_metadata(
+        lpad, mongo_jobstore, fw_dir, simple_flow, connected_flow, capsys
+):
     from fireworks.core.rocket_launcher import rapidfire
 
     from jobflow.managers.fireworks import flow_to_workflow
@@ -234,14 +236,10 @@ def test_simple_flow_metadata(lpad, mongo_jobstore, fw_dir, simple_flow, connect
     rapidfire(lpad)
 
     result = mongo_jobstore.query_one({"uuid": uuid0})
-    fw_id = list(fw_ids.values())[0]
-    assert result["metadata"] == {
-        "fw_id": fw_id, "tags": ["some tag", "my_flow"]
-    }
+    assert result["metadata"]["tags"] == ["some tag", "my_flow"]
 
     result = mongo_jobstore.query_one({"uuid": uuid1})
-    fw_id = list(fw_ids.values())[1]
-    assert result["metadata"] == {"fw_id": fw_id, "tags": ["tag, you're it", "my_flow"]}
+    assert result["metadata"]["tags"] == ["tag, you're it", "my_flow"]
 
 
 def test_connected_flow(lpad, mongo_jobstore, fw_dir, connected_flow, capsys):
