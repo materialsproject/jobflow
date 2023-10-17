8ed03672bfe7947b76d49831f1462338d875d1ad
Janosh Riebesell <janosh.riebesell@gmail.com>
2023-10-17 15:42:04 -0700

    coverage ignore PackageNotFoundError in _version.py

--- src/jobflow/_version.py
+++ src/jobflow/_version.py
@@ -2,6 +2,6 @@ from importlib.metadata import PackageNotFoundError, version
 
 try:
     __version__ = version("jobflow")
-except PackageNotFoundError:
+except PackageNotFoundError:  # pragma: no cover
     # package is not installed
     __version__ = ""
