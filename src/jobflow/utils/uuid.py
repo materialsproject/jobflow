7f99f00c9e759c0922d8e68a92ae7b78ef850de6
@jmmshn <jmmshn@gmail.com>
2024-01-20 10:10:34 -0800

    deprecate suuid

---
+++ src/jobflow/utils/uuid.py
@@ -0,0 +1,20 @@
+"""Tools for generating UUIDs."""
+from monty.dev import deprecated
+
+
+@deprecated(
+    message="The UUID system will be replace with UID that contains both UUID and ULID."
+)
+def suuid() -> str:
+    """Generate a string UUID (universally unique identifier).
+
+    Uses the UUID4 specification.
+
+    Returns
+    -------
+    str
+        A UUID.
+    """
+    from uuid import uuid4
+
+    return str(uuid4())
